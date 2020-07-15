2e0571189717e5906163b408e7b1ced917355f07
Cristian Vega <cris.vega.03@gmail.com>
2020-07-14 21:17:10 -0500

    docs added

---
+++ docs/precache-manifest.3c714c40e7ae6c0e281c6ce1923d8402.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf5a9c62a773f81c8a3053048c36c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22dd7aa55ed897b00980",
+    "url": "/static/css/main.00be4a9b.chunk.css"
+  },
+  {
+    "revision": "e8065e11b57ea0a8b288",
+    "url": "/static/js/2.20b7e593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20b7e593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dd7aa55ed897b00980",
+    "url": "/static/js/main.f2d9c648.chunk.js"
+  },
+  {
+    "revision": "38624478660ef4d56b2e",
+    "url": "/static/js/runtime-main.2b0b66a4.js"
+  },
+  {
+    "revision": "a485f254c8bb8601eea3d3e40f0275d2",
+    "url": "/static/media/IMG_1629.a485f254.jpg"
+  },
+  {
+    "revision": "778232f33c016ccf27774b21c9f8d5fe",
+    "url": "/static/media/hero.778232f3.jpg"
+  },
+  {
+    "revision": "a2c218a0e7508e6ce64a3713d86e1d53",
+    "url": "/static/media/logo.a2c218a0.jpg"
+  }
+]);
